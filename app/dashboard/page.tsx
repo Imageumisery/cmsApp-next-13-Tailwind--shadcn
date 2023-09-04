69b0f7cc3a548b439d881e5cdcfe8ca028a9b31c
Imageumisery <imageumisery@gmail.com>
2023-09-05 00:07:13 +0500

    clerk initialized

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+"use client";
 
-type Props = {}
+import { Button } from "@/components/ui/button";
+import { Modal } from "@/components/ui/modal";
+import React from "react";
+
+type Props = {};
 
 const layout = (props: Props) => {
-  return (
-    <div>My dashboard</div>
-  )
-}
+    return (
+        <div className="p-4">
+            <Modal isOpen description="Test desc" onClose={() => {  }} title="Some title" ></Modal>
+            <Button size="lg" variant="destructive">
+                Make the world beautiful again!
+            </Button>
+        </div>
+    );
+};
 
-export default layout
+export default layout;
---
+++ hooks/useStoreModal.tsx
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+
+interface useStoreModalInterfaceStore {
+    isOpen: boolean;
+    onOpen: () => void;
+    onClose: () => void;
+}
+
+export const useStoreModal = create<useStoreModalInterfaceStore>((set) => ({
+    isOpen: false,
+    onOpen: () => {
+        set({ isOpen: true });
+    },
+    onClose: () => {
+        set({ isOpen: true });
+    },
+}));
